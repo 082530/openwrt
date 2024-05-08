4a9ec001697ed356547ab4d20502f58a8f673588
082530 <38971082+082530@users.noreply.github.com>
2024-05-08 14:25:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,18 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generat
+#=========================================================================================================
+        cd openwrt
+        # 添加的 feeds 应用包优先于自带的 feed 里的 app
+        echo "重复的包检测：👇"
+        ./scripts/feeds list  | awk '{if(a[$1]){print $1}else{a[$1]++}}'
+        echo "重复的包检测：👆"
+        ./scripts/feeds list  | awk '{if(a[$1]){print $1}else{a[$1]++}}' | while read pkg_name;do
+            # 目录是 / 分隔，feeds/xxx/ 一样就不打印
+            find feeds/ -maxdepth 4 -type d -name $pkg_name | \
+              awk -F/ 'NR==1{a[$2]=$0};NR==2{if(!a[$2]){for(i in a){if(a[i]){printf "%s/ %s\n",$0,a[i]}}}}' | \
+              xargs -r -n2 echo  👉 rsync -av --delete
+            find feeds/ -maxdepth 4 -type d -name $pkg_name | \
+              awk -F/ 'NR==1{a[$2]=$0};NR==2{if(!a[$2]){for(i in a){if(a[i]){printf "%s/ %s\n",$0,a[i]}}}}' | \
+              xargs -r -n2 rsync -av --delete
+        done
